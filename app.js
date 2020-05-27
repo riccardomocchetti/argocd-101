33eed4480dc0251a0d5d65159087ca9e192bb5e0
Riccardo Mocchetti <riccardo.mocchetti@container-solutions.com>
2020-05-27 14:15:30 +0100

    changed title color to blue

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World<\h1>');
+  res.end('<h1 style="color:blue">Hello World</h1>');
 });
 
 server.listen(port, hostname, () => {
